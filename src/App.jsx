e041ae7ecd7f962730714a2a36fd341a4f197303
Mustafa Ali <mustafaaaly27@gmail.com>
2024-08-21 01:07:25 +0500

    first attempt topics (map ternary operator jsx extension and writing markup using js

---
+++ src/App.jsx
@@ -0,0 +1,125 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const products = [
+  {
+    "id": 1,
+    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+  },
+  {
+    "id": 2,
+    "title": "Mens Casual Premium Slim Fit T-Shirts ",
+    "price": 22.3,
+    "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    "rating": {
+      "rate": 4.1,
+      "count": 259
+    }
+  },
+  {
+    "id": 3,
+    "title": "Mens Cotton Jacket",
+    "price": 55.99,
+    "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    "rating": {
+      "rate": 4.7,
+      "count": 500
+    }
+  },
+  {
+    "id": 4,
+    "title": "Mens Casual Slim Fit",
+    "price": 15.99,
+    "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    "rating": {
+      "rate": 2.1,
+      "count": 430
+    }
+  }]
+
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+const conditionForternaryOpt = false
+  return (
+    // <>
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </>
+// we can write html markups here 
+
+
+
+// we can only do it inside single element and it should be a closing tag i.e <br/> 
+// instead of class we use className  with camel case practice or we can also use html to jsx converter to don't need to do anything like that else we have to follow all the rules
+
+<>
+{/* <h1>Hello World</h1> */}
+{/* <input type="text" placeholder='Enter Your Name' className='nameInput' /> */}
+<button>{conditionForternaryOpt ? "Ye tru hogya tou login" : "React hogyi complete :D"}</button>
+<h1>Products :</h1>
+
+
+
+
+{products.map((product, index) => (
+  <img key={index} src={product.image}  height="200px" width="100px" /> // here key works as an id which is dynamic cuz index number change horha so ... 
+  
+  
+))}
+
+
+
+
+{/* {conditionForternaryOpt ? "Ye tru hogya tou login" : "false hogya means logout"} */}
+{/* what is this? */}
+{/* agr condition true hogyi tou phla wrna second wala chlta hai : iske baad wala  */}
+{/* its short form for if else condition  */}
+
+</>
+
+
+
+
+
+
+
+  )
+}
+
+export default App
